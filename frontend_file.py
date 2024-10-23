570a93f0cb83e2d8cdf319ae649c7b2dea9008b0
Jean-Michel Lejeune <jean-michel.lejeune@evolvi.be>
2024-10-23 11:49:15 +0200

    Initial commit with Streamlit frontend

---
+++ frontend_file.py
@@ -0,0 +1,28 @@
+#url: https://iris-370638329385.europe-west1.run.app/
+
+import streamlit as st
+import requests
+
+ # Change this URL to the one of your API
+#API_URL = "https://my-api-app-1234567891234.europe-west1.run.app/" -> lewagon API
+API_URL = "https://iris-370638329385.europe-west1.run.app/"
+st.title("My awesome MVP")
+
+st.write("Iris predictor")
+
+sepal_length = st.slider('Select a value for Sepal length', min_value=0, max_value=4, value=1, step=1)
+sepal_width = st.slider('Select a value for Sepal width',  min_value=0, max_value=4, value=1, step=1)
+petal_length = st.slider('Select a value for Petal length',  min_value=0, max_value=4, value=1, step=1)
+petal_width = st.slider('Select a value for Petal width',  min_value=0, max_value=4, value=1, step=1)
+
+url = f"{API_URL}/predict"
+params = {
+    'sepal_length': sepal_length,
+    'sepal_width': sepal_width,
+    'petal_length': petal_length,
+    'petal_width': petal_width,
+}
+
+response = requests.get(url, params=params).json()
+
+st.write(f"This flower belongs to category {str(response['prediction'])}")
